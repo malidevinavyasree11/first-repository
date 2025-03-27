6d29360de05fbcd24a22b14a1a69c4ca579802d3
Navya Sree <malidevi.navya-sree@capgemini.com>
2025-03-27 15:49:09 +0530

    table1 created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
-//import Table from './Table/Table'
-import Userlist from './User/Userlist'
+import Table1 from './Table/Table1'
+//import Userlist from './User/Userlist'
 const App = () => {
   return (
     <div>
-      <h1><Userlist/></h1>
+      <h1><Table1/></h1>
     </div>
   )
 }
---
+++ src/Table/Table1.js
@@ -0,0 +1,39 @@
+import React,{useEffect,useState} from 'react'
+import './Table.css'
+function App(){
+    const[data,setData]=useState([])
+    useEffect(()=>{
+      fetch("https://dummyjson.com/users")
+      .then((response)=>response.json())
+      .then((json)=>setData(json.users))
+    },[])
+    return(
+        <table id="datatable">
+            <tr>
+            <th>Id</th>
+            <th>FirstName</th>
+            <th>LastName</th>
+            <th>UserName</th>
+             <th>Password</th>
+             <th>PhoneNumber</th>
+             <th>Email</th>
+              <th>Gender</th> 
+            </tr>
+            <tbody>
+                {data.map((item)=>(
+                   <tr key={item}>
+                   <td>{item.id}</td>
+                   <td>{item.firstName}</td>
+                   <td>{item.lastName}</td>
+                   <td>{item.username}</td>
+                   <td>{item.password}</td>
+                   <td>{item.phone}</td>
+                   <td>{item.email}</td>
+                   <td>{item.gender}</td>
+                </tr>
+                ))}               
+            </tbody>
+        </table>
+    )
+}
+export default App;
